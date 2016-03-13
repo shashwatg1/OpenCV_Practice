612e48864741ec2883257237950ffc9aec36269c
shashwatg1 <shashwatg1@gmail.com>
2016-03-13 22:47:58 +0530

    IP winter work

---
+++ ip_winter_work/build/CMakeFiles/compiled_sixth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/compiled_sixth.dir/sixth.cpp.o"
+  "compiled_sixth.pdb"
+  "compiled_sixth"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/compiled_sixth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
